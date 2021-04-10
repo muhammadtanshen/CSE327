051a78930c73fddebba94e7662041c7c76de12d1
Mehedi <mmehedihasan514@gmail.com>
2021-04-10 10:18:05 +0600

    update product route file

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,23 +3,11 @@ const asyncHandler = require('express-async-handler');
 const router = express.Router();
 
 const Product  =  require('../models/productModel');
+const productController = require('../controllers/productController');
 
-router.get('/',asyncHandler(async(req,res)=>{
-    const products = await Product.find({});
-    //throw new Error('some error');
-    res.json(products);
-}))
+router.get('/',productController.getAllProduct);
 
-router.get('/:id',asyncHandler(async(req,res)=>{
-    const product = await Product.findById(req.params.id);
-    
-    if(product){
-        res.json(product);
-    }else{
-        res.status(400);
-        throw new Error('Product not found.');
-    }
-}))
+router.get('/:id',productController.getSingleProduct);
 
 
 module.exports = router;
