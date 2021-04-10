f3f8484ac2810c1bd38077c43108ac047145d40e
Mehedi <mmehedihasan514@gmail.com>
2021-04-10 09:58:51 +0600

    update server file

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,34 @@
-const expresss = require('express');
+const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('../backend/config/db');
 const colors = require('colors');
 const products = require('../backend/data/products');
 const productRoutes = require('./routes/productRoutes');
+const userRoutes = require('./routes/userRoutes');
+const orderRoutes = require('./routes/orderRoutes');
 const errorMiddleWare = require('../middlewares/errorMiddleWare');
+
+
 
 dotenv.config();
 connectDB();
-const app = expresss();
+const app = express();
 
-app.use((req,res,next)=>{
-    console.log(req.originalUrl);
-    next();
-})
+// app.use((req,res,next)=>{
+//     console.log(req.originalUrl);
+//     next();
+// })
+
+app.use(express.json());
+
 
 app.get('/',(req,res)=>{
     res.send('Hello I am the main page..');
 })
 
 app.use('/api/products',productRoutes);
-
+app.use('/api/users',userRoutes);
+app.use('/api/orders',orderRoutes);
 
 app.use(errorMiddleWare.notFound);
 
