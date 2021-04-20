2cdb709b0748ead25ab7eebd2ee421164fc0f919
MD Mehedi Hasan <48857205+MDMEHEDIHASA@users.noreply.github.com>
2021-04-20 19:06:39 +0600

    Delete adminUser.js

--- middlewares/adminUser.js
+++
@@ -1,8 +0,0 @@
-exports.admin = (req,res,next)=>{
-    if(req.user && req.user.isAdmin){
-       next(); 
-    }else{
-        res.status(401);
-        throw new Error("NOt autorize or NOt a admin😡🗯.")
-    }
-}
