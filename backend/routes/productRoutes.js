2b3815906c94dc68a547ee9a161f634e9f922ef2
MD Mehedi Hasan <48857205+MDMEHEDIHASA@users.noreply.github.com>
2021-04-20 19:38:24 +0600

    Complete product Routes file

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,13 +1,30 @@
 const express = require('express');
-const asyncHandler = require('express-async-handler');
 const router = express.Router();
 
 const Product  =  require('../models/productModel');
 const productController = require('../controllers/productController');
+const protectAuthMiddleWare = require('../../middlewares/authMiddlware');
+const adminUserMiddlWare = require('../../middlewares/adminUser');
+
+//top rated products
+router.get('/top',productController.getTopProducts);
 
 router.get('/',productController.getAllProduct);
 
 router.get('/:id',productController.getSingleProduct);
 
 
+
+//product review
+router.post('/:id/reviews',protectAuthMiddleWare,productController.createProductReview);
+
+
+
+//delete product
+router.delete('/:id',protectAuthMiddleWare,adminUserMiddlWare.admin,productController.deleteProduct)
+
+//cereate product
+router.post('/',protectAuthMiddleWare,adminUserMiddlWare.admin,productController.createProduct);
+//Update product
+router.put('/:id',protectAuthMiddleWare,adminUserMiddlWare.admin,productController.updateProduct);
 module.exports = router;
