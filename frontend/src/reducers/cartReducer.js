63a5727c8e0873741dfdf8411cd0d77593842f16
MD Mehedi Hasan <48857205+MDMEHEDIHASA@users.noreply.github.com>
2021-04-20 19:23:06 +0600

    Add Remove cart and item product

--- frontend/src/reducers/cartReducer.js
+++ frontend/src/reducers/cartReducer.js
@@ -1,29 +1,53 @@
-import {CART_ADD_ITEM,CART_REMOVE_ITEM} from '../components/constants/cartConstant';
+import {
+  CART_ADD_ITEM,
+  CART_REMOVE_ITEM,
+  CART_SAVE_SHIPPING_ADDRESS,
+  CART_SAVE_PAYMENT_METHOD,
+  CART_RESET
+} from "../components/constants/cartConstant";
 
-export const cartReducer = (oldlistCart={cartItems:[]},action)=>{
-    switch(action.type){
-        case CART_ADD_ITEM:
-            const item = action.payload;
-            const existItem = oldlistCart.cartItems.find((x)=>x.productId === item.productId);
-            if(existItem){
-                return{
-                    ...oldlistCart,
-                    cartItems: oldlistCart.cartItems.map(x=> x.productId === existItem.productId ? item : x)
-                }
-            }else{
-                return{
-                    ...oldlistCart,
-                    cartItems:[...oldlistCart.cartItems,item]
-                }
-            }
-        case CART_REMOVE_ITEM:
-            return{
-                ...oldlistCart,
-                cartItems:oldlistCart.cartItems.filter(x=>{
-                    return x.productId !== action.payload;
-                })
-            }
-        default:
-            return oldlistCart;
-    }
-}
+export const cartReducer = (oldlistCart = { cartItems: [],shippingAddress:{} }, action) => {
+  switch (action.type) {
+    case CART_ADD_ITEM:
+      const item = action.payload;
+      const existItem = oldlistCart.cartItems.find(
+        (x) => x.productId === item.productId
+      );
+      if (existItem) {
+        return {
+          ...oldlistCart,
+          cartItems: oldlistCart.cartItems.map((x) =>
+            x.productId === existItem.productId ? item : x
+          ),
+        };
+      } else {
+        return {
+          ...oldlistCart,
+          cartItems: [...oldlistCart.cartItems, item],
+        };
+      }
+    case CART_REMOVE_ITEM:
+      return {
+        ...oldlistCart,
+        cartItems: oldlistCart.cartItems.filter((x) => {
+          return x.productId !== action.payload;
+        }),
+      };
+    case CART_SAVE_SHIPPING_ADDRESS:
+      return {
+        ...oldlistCart,
+        shippingAddress: action.payload,
+      };
+      case CART_SAVE_PAYMENT_METHOD:
+      return {
+        ...oldlistCart,
+        paymentMethod: action.payload,
+      }
+      case CART_RESET:
+        return{
+          oldlistCart:{cartItems:[]}
+        }
+    default:
+      return oldlistCart;
+  }
+};
