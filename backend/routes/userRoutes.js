248885c3790c77b5cfbeac3b359c5f9f9fa18a28
MD Mehedi Hasan <48857205+MDMEHEDIHASA@users.noreply.github.com>
2021-04-20 19:39:20 +0600

    Complete User Routes file

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,9 +3,18 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 const protectAuthMiddlware = require('../../middlewares/authMiddlware');
 const User = require('../models/userModel');
+const adminUserMiddlWare = require('../../middlewares/adminUser');
+
 
 router.post('/',userController.registerUser);
 router.post('/login',userController.authUser);
 router.get('/profile',protectAuthMiddlware,userController.getUserProfile);
 router.put('/profile',protectAuthMiddlware,userController.updateUserProfile);
+
+router.get('/',protectAuthMiddlware,adminUserMiddlWare.admin,userController.getUsers);
+router.delete('/:id',protectAuthMiddlware,adminUserMiddlWare.admin,userController.deleteUser);
+router.get('/:id',protectAuthMiddlware,adminUserMiddlWare.admin,userController.getUserById);
+router.put('/:id',protectAuthMiddlware,adminUserMiddlWare.admin,userController.updateUser);
+
+
 module.exports = router;
